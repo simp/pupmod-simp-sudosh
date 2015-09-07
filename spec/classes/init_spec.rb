b9793a5357b12fa8d9430574a96b2a1cf898264c
nick-markowski <nmarkowski@keywcorp.com>
2015-09-07 19:13:10 +0000

    (SIMP-437) Update pupmod-simp-sudosh for skeleton
    Easiest way to ensure travis tests pass was to update the metadata
    to match the skeleton.
    
    SIMP-437 #close Update sudosh for skeleton.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,22 +2,33 @@ require 'spec_helper'
 
 describe 'sudosh' do
 
-  it { should create_class('sudosh') }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) do
+        facts
+      end
 
-  it { should contain_package('sudosh2') }
+      context "on #{os}" do
+        it { is_expected.to create_class('sudosh') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_package('sudosh2') }
 
-  it do
-    should contain_rsyslog__add_rule('0sudosh').with({
-      'rule' => "\n\nif \$programname == 'sudosh' then \t\t /var/log/sudosh.log\n& ~\n"
-    })
+        it do
+          is_expected.to contain_rsyslog__rule__local('0sudosh').with({
+            'rule' => "if ($programname == \'sudosh\') then",
+            'target_log_file' => '/var/log/sudosh.log',
+            'stop_processing' => true
+          })
+        end
+
+        it do
+          is_expected.to contain_logrotate__add('sudosh').with({
+            'log_files' => ['/var/log/sudosh.log'],
+            'missingok' => true,
+            'lastaction' => '/sbin/service rsyslog restart > /dev/null 2>&1 || true'
+          })
+        end
+      end
+    end
   end
-
-  it do
-    should contain_logrotate__add('sudosh').with({
-      'log_files' => '/var/log/sudosh.log',
-      'missingok' => true,
-      'lastaction' => '/sbin/service rsyslog restart > /dev/null 2>&1 || true'
-    })
-  end
-
 end
