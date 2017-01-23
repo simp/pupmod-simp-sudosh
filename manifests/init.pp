efec4df7674c4d2a143e9a2e8bdc6ef787a8a3ec
Nick Miller <nick.miller@onyxpoint.com>
2017-01-23 14:47:25 -0500

    (SIMP-2512) Remove logic from rsyslog rule
    - Update rsyslog rule to remove now-implied logic
    
    SIMP-2512 #close

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,9 @@
 #   to create a log rotate rule for the sudosh log file.
 #
 # @example Ensuring all sudo operations are run with sudosh
-#   Insert the following code in an appropriate manifest (e.g., 
+#   Insert the following code in an appropriate manifest (e.g.,
 #   /etc/puppet/manifests/nodes/default_classes/base_config.pp).
-# 
+#
 #     sudo::user_specification { 'global_admin':
 #       user_list => '%administrators',
 #       host_list => 'ALL',
@@ -23,10 +23,11 @@
 # @author Trevor Vaughan <tvaughan@onyxpont.com>
 #
 class sudosh (
-  Boolean $syslog      = simplib::lookup('simp_options::syslog', { 'default_value' => false }),
-  Boolean $logrotate   = simplib::lookup('simp_options::logrotate', { 'default_value' => false })
+  Boolean $syslog    = simplib::lookup('simp_options::syslog', { 'default_value' => false }),
+  Boolean $logrotate = simplib::lookup('simp_options::logrotate', { 'default_value' => false })
 ) {
 
+  # This package is from the SIMP repo
   package { 'sudosh2':
     ensure => 'latest'
   }
@@ -35,7 +36,7 @@ class sudosh (
     include '::rsyslog'
     # named 'XX_sudosh' so that it appears before the local filesystem defaults
     rsyslog::rule::local { 'XX_sudosh':
-      rule            => 'if ($programname == \'sudosh\') then',
+      rule            => '$programname == \'sudosh\'',
       target_log_file => '/var/log/sudosh.log',
       stop_processing => true
     }
