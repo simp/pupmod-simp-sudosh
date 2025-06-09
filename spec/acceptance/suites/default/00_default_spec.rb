bde3ab3b1e6958c8d7c3df371c1c9342d5728468
Steven Pritchard <steve@sicura.us>
2025-06-09 14:58:29 -0500

    Clean up for rubocop (#78)
    * Clean up for rubocop
    
    ---------
    
    Co-authored-by: Chris Tessmer <chris.tessmer@onyxpoint.com>

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -3,28 +3,30 @@ require 'spec_helper_acceptance'
 test_name 'sudosh'
 
 describe 'sudosh' do
-  let(:manifest) {
-    <<-EOS
+  let(:manifest) do
+    <<~EOS
       include 'sudosh'
     EOS
-  }
+  end
 
   hosts.each do |host|
     context "on #{host}" do
       context 'default parameters' do
-        it 'should enable SIMP dependencies repo for sudosh package' do
+        it 'enables SIMP dependencies repo for sudosh package' do
           install_simp_repos(host)
         end
 
-        it 'should work with no errors' do
-          apply_manifest_on(host, manifest, :catch_failures => true)
+        # rubocop:disable RSpec/RepeatedExample
+        it 'works with no errors' do
+          apply_manifest_on(host, manifest, catch_failures: true)
         end
 
-        it 'should be idempotent' do
-          apply_manifest_on(host, manifest, :catch_failures => true)
+        it 'is idempotent' do
+          apply_manifest_on(host, manifest, catch_failures: true)
         end
+        # rubocop:enable RSpec/RepeatedExample
 
-        it 'should have sudosh2 installed' do
+        it 'has sudosh2 installed' do
           expect(check_for_package(host, 'sudosh2')).to be true
         end
       end
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe 'sudosh' do
-
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       let(:facts) do
@@ -13,30 +12,31 @@ describe 'sudosh' do
           it { is_expected.to create_class('sudosh') }
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_package('sudosh2') }
-          it { is_expected.to_not contain_rsyslog__rule__local('XX_sudosh') }
-          it { is_expected.to_not contain_logrotate__rule('sudosh') }
+          it { is_expected.not_to contain_rsyslog__rule__local('XX_sudosh') }
+          it { is_expected.not_to contain_logrotate__rule('sudosh') }
         end
 
         context 'with syslog and logrotate enabled' do
-          let(:params) {{ :syslog => true, :logrotate => true }}
+          let(:params) { { syslog: true, logrotate: true } }
+
           it { is_expected.to create_class('sudosh') }
           it { is_expected.to compile.with_all_deps }
           it { is_expected.to contain_package('sudosh2') }
 
           it do
-            is_expected.to contain_rsyslog__rule__local('XX_sudosh').with({
-              'rule' => "$programname == \'sudosh\'",
+            is_expected.to contain_rsyslog__rule__local('XX_sudosh').with(
+              'rule'            => "$programname == \'sudosh\'",
               'target_log_file' => '/var/log/sudosh.log',
-              'stop_processing' => true
-            })
+              'stop_processing' => true,
+            )
           end
 
           it do
-            is_expected.to contain_logrotate__rule('sudosh').with({
+            is_expected.to contain_logrotate__rule('sudosh').with(
               'log_files'                 => ['/var/log/sudosh.log'],
               'missingok'                 => true,
-              'lastaction_restart_logger' => true
-            })
+              'lastaction_restart_logger' => true,
+            )
           end
         end
       end
